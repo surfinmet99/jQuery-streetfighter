b35f806ef023ed44d2c16221b8234ce12b7dc674
Chad Rich <surfinmet99@yahoo.com>
2015-06-22 20:06:40 -0400

    Added animation and text

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left', '520px');
+			});
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+
+	$('.ryu').keydown(function() {
+		$('.ryu-ready').hide();
+		/*$('.ryu-still').hide();*/
+		$('.ryu-cool').show();	
+	});
+
+	$('.ryu').keyup(function() {
+		$('.ryu-cool').hide();
+		$('.ryu-ready').show();
+	});
+
+});
+
+
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
